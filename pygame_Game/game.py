926b0a5a2f35dbf1e10644a60a127c8e97324332
Ole-Marcus-Hansen <olemarhan@gmail.com>
2025-01-28 14:41:24 +0100

    make grid stuff

--- pygame_Game/game.py
+++ pygame_Game/game.py
@@ -1,7 +1,19 @@
 import jellygame
 
 window = jellygame.Window("game window")
-window.create_grid(10, 10)
+window.create_grid(6, 6)
 
-window.draw_grid()
+for x in range(0, 6):
+    for y in range(0, 6):
+        window.tiles[y][x].set_color(jellygame.color.GRAY)
+
+for x in range(1, 5):
+    for y in range(1, 5):
+        window.tiles[y][x].set_color(jellygame.color.WHITE)
+
+
+text1 = jellygame.Text("0", 5, 0, 1, 1, 24, background_color=jellygame.color.GRAY)
+window.add_text(text1)
+
+window.draw_grid(tile_size=100, show_grid=False)
 window.start()
